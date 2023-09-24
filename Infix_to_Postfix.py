7c334f25b899da34634408dfd86c6af7c692c5a7
reeuze <adittitik@gmail.com>
2023-09-25 03:10:53 +0700

    Commit 3 (func Convert Succes)

--- Infix_to_Postfix.py
+++ Infix_to_Postfix.py
@@ -1,7 +1,6 @@
 class Infix_Postfix:
     def __init__(self):
-        self.Number = []
-        self.Character = []
+        self.Input = []
         self.Temp = []
         self.Postfix = []
         self.Sum = 0
@@ -15,19 +14,14 @@ class Infix_Postfix:
         return a/b
     def Rank(self, a, b):
         return a^b
-    def Priority(self, var):
-        if var==')':
-            return var
-        elif var=='(':
-            print("A")
-    def Convert(self, String):
-        for char in String:
-            if char.isdigit():
-                self.Number.append(char)
-            else:
-                self.Temp.append(char)
-
-    # def Postfix(self, var):
+    def Convert (self, input):
+        for char in input:
+            self.Input.append(char)
+    def Algorithm(self, input=[], index=0):
+        if input[index].isDigit() or input[index]=='(':
+            self.Algorithm(input, index+1)
+        
 
 input = "1+(2/3-(4*5^6)+7)*8"
 a = Infix_Postfix()
+a.Convert(input)
