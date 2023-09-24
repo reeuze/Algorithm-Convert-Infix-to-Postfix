d8d25882975f4a6b763333a7465ad6587b140c40
reeuze <adittitik@gmail.com>
2023-09-25 04:35:48 +0700

    Commit 5 (Fix Readme.md & failed func Algorithm)

--- Infix_to_Postfix.py
+++ Infix_to_Postfix.py
@@ -1,26 +1,53 @@
 class Infix_Postfix:
     def __init__(self):
         self.Input = []
-        self.Temp = []
+        self.Number = []
+        self.Stack = []
         self.Postfix = []
         self.Sum = 0
-    def Summation(self, a, b):
-        return a+b
-    def Subtraction(self, a, b):
-        return a-b
-    def Multiplication(self, a, b):
-        return a*b
-    def Distribution(self, a, b):
-        return a/b
-    def Rank(self, a, b):
-        return a^b
-    def Convert (self, input):
+    def Print(self, input):
+        print("scanned input : ", input)
+        print("Stack :", ''.join(self.Stack))
+        print("Postfix :", ''.join(self.Postfix), "\n")
+    def Convert(self, input):
         for char in input:
             self.Input.append(char)
+    def Is_Operator(self, character):
+        operators = "+-*/^"
+        if character in operators:
+            return True
+    def Operation(self, var1, var2, operator):
+        if operator=='+':
+            return var1+var2
+        elif operator=='-':
+            return var1-var2
+        elif operator=='*':
+            return var1*var2
+        elif operator=='/':
+            return var1/var2
+        elif operator=='^':
+            return var1^var2
+        return None
     def Algorithm(self, input=[], index=0):
-        if input[index].isDigit() or input[index]=='(':
+        if input[index].isDigit():
+            self.Postfix.append(input[index])
+            # self.Algorithm(input, index+1)
+            sum = self.Operation(input[index], self.Algorithm(input, index+2), self.Algorithm(input, index+1))
+            if sum is None:
+                return input[index]
+            else:
+                return sum
+        elif input[index]=='(':
+            self.Stack.append(input[index])
             self.Algorithm(input, index+1)
-        
+        elif self.Is_Operator(input[index]) is True:
+            
+            self.Stack.append(input[index])
+        elif input[index]==')':
+            return input[index-1]
+        elif input[index]==input[-1]:
+            return input[index]
+        self.Print(input[index])
 
 input = "1+(2/3-(4*5^6)+7)*8"
 a = Infix_Postfix()
--- Readme.md
+++ Readme.md
@@ -4,12 +4,13 @@ Assesment #4
 Create a program that will change from infix to postfix
 This is the example of Infix convert to Postfix :
 
---- photo ---
+-- photo --
 
 The algorithm :
 1. Ubah input dari bentuk string menjadi stack input
 2. Lakukan pemanggilan terhadap def Algorithm (self, input)
 3. Cek apakah input merupakan digit atau tidak
-4. a. Apabila input adalah digit atau "(", maka panggil def Algorithm (self, input)
-   b. Apabila input adalah operator, maka lakukan operasi hitung dan return bentuk postfix ke stack Temp
-      setelah itu return hasil operasinya
+4. Apabila input[index] adalah digit atau "(", maka panggil def Algorithm (self, input)
+5. Apabila input[index] adalah operator, maka lakukan operasi hitung dan return bentuk
+postfix ke stack Temp setelah itu return hasil operasinya
+6. Apabila input[index] adalah ")" atau input[-1]
