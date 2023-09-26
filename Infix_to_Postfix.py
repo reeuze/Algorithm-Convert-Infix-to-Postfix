baf0f310394b374d2c64b623f1975eddbfa853e4
reeuze <adittitik@gmail.com>
2023-09-26 11:34:21 +0700

    Commit 9 (still failed)

--- Infix_to_Postfix.py
+++ Infix_to_Postfix.py
@@ -1,14 +1,13 @@
 class Infix_Postfix:
     def __init__(self):
         self.Input = []
+        self.Temp = []
         self.Number = []
-        self.Stack = []
+        self.Character = []
         self.Postfix = []
-        self.Operator = '+'
-        self.Sum = 0
     def Print(self, input):
         print("scanned input : ", input)
-        print("Stack :", ''.join(self.Stack))
+        print("Stack :", ''.join(self.Character))
         print("Postfix :", ''.join(self.Postfix), "\n")
     def Convert(self, input):
         self.Input.append('(')
@@ -31,35 +30,47 @@ class Infix_Postfix:
         elif operator=='^':
             return var1^var2
         return None
-    def Priority(self, operator1, operator2):
+    def Priority(self, index):
         operators = [['+', '-'], ['*', '/'], ['^']]
-        if operator1.index(operators) > operator2.index(operators):
-            return operator1
-        elif operator1.index(operators) < operator2.index(operators):
-            return operator2
-        elif operator1.index(operators) == operator2.index(operators):
-            return operator1
+        for i, operator in range(index, len(self.Temp)):
+            for j in range(0, 2):
+                if operator==operators(3):
+                    self.Number[i] = self.Operation(self.Number.pop[i], self.Number.pop[i+1], self.Temp.pop[i])
+                elif operator==operators(2):
+                    self.Number[i] = self.Operation(self.Number.pop[i], self.Number.pop[i+1], self.Temp.pop[i])
+                elif operator==operators(2):
+                    self.Number[i] = self.Operation(self.Number.pop[i], self.Number.pop[i+1], self.Temp.pop[i])
+        return
     def Algorithm(self, input=[], index=0):
-        if not self.Stack:
-            self.Stack.append('(')
-            self.Algorithm(input, index)
-        elif input[index]=='(':
-            self.Stack.append(input[index])
-            # self.Print(input[index])
-            self.Algorithm(input, index+1)
-        elif input[index].isdigit():
-            self.Postfix.append(input[index])
-            # self.Print(input[index])
-        elif self.Is_Operator(input[index]) is True:
-            self.Priority(input[index], self.Stack[-1])
-            self.Stack.append(input[index])
-        elif input[index]==')':
-            return input[index-1]
-        elif input[index]==input[-1]:
-            return input[index]
+        digit = 0
+        digit_in = 0
+        for i in range(index, len(input)):
+            if input[index]=='(':
+                self.Character.append(input[i])
+                self.Print(input[i])
+                self.Algorithm(input, i+1)
+                digit += 1
+            # disini bagian perhitunganya
+            elif input[index].isdigit():
+                self.Number.append(input[index])
+                self.Postfix.append(input[index])
+                self.Print(input[index])
+                digit += 1
+                digit_in += 1
+            elif self.Is_Operator(input[index]) is True:
+                self.Temp.append(input[index])
+                self.Character.append(input[index])
+            elif input[index]==')':
+                self.Priority(digit-digit_in-1)
+                return
+            elif input[index]==input[-1]:
+                self.Priority(digit-digit_in-1)
+                return
+            
         self.Print(input[index])
 
 input = "1+(2/3-(4*5^6)+7)*8"
 a = Infix_Postfix()
 a.Convert(input)
 a.Algorithm(a.Input)
+print(''.join(a.Number))
