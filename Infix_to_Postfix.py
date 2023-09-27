51f80b8ef3022931e2748c362e0c84117d550e40
reeuze <adittitik@gmail.com>
2023-09-27 12:23:22 +0700

    Commit 13 (still failed)

--- Infix_to_Postfix.py
+++ Infix_to_Postfix.py
@@ -8,8 +8,10 @@ class Infix_Postfix:
         print("Stack :", ''.join(self.Character))
         print("Postfix :", ''.join(self.Postfix))
     def Convert(self, input):
+        self.Input.append('(')
         for char in input:
             self.Input.append(char)
+        self.Input.append(')')
     def Is_Operator(self, character):
         operators = "+-*/^"
         if character in operators:
@@ -25,11 +27,23 @@ class Infix_Postfix:
             return int(var1)/int(var2)
         elif operator=='^':
             return int(var1)**int(var2)
-    def Priority(self, Temp=[], Number=[]):
-        while len(Number) != 1:
-            best = 0
-            i = 0
-            for j, operator in enumerate(Temp):
+    def Priority(self, input=[], index=0):
+        self.Char = []
+        self.Num = []
+        # print(input, len(input))
+        while input:
+            # print(index, input[index])
+            if input[index]==')':
+                break
+            elif input[index].isdigit():
+                self.Num.append(input.pop(index))
+            elif self.Is_Operator(input[index]) is True:
+                self.Char.append(input.pop(index))
+            # print(self.Char, self.Num)
+        i = 0
+        best  = 0 
+        while len(self.Num) != 1:
+            for j, operator in enumerate(self.Char):
                 if operator=='+' or operator=='-' and best < 1:
                     n = 1
                     i = j
@@ -40,45 +54,62 @@ class Infix_Postfix:
                     n = 3
                     i = j
                 best = n
-            print("var1 :", Number[i], "var2 :", Number[i+1], "oper :", Temp[i])
-            hasil = self.Operation(Number.pop(i), Number.pop(i), Temp.pop(i))
-            Number.append(hasil)
-        return Number.pop()
+            # print(i)
+            # print("var1 :", self.Num[i], "var2 :", self.Num[i+1], "oper :", self.Char[i])
+            hasil = self.Operation(self.Num.pop(i), self.Num.pop(i), self.Char.pop(i))
+            self.Num.append(hasil)
+        return input[index-1]
+    def Replace_operator(self, op1, op2):
+        i = 0
+        j = 0
+        if op1=='+' or op1=='-':
+            i = 1
+        elif op1=='*' or op1=='/':
+            i = 2
+        elif op1=='^':
+            i = 3
+        elif op2=='+' or op2=='-':
+            j = 1
+        elif op2=='*' or op2=='/':
+            j = 2
+        elif op2=='^':
+            j = 3
+        print("i = ", i, "j = ", j)
+        if i>=j:
+            return op1
+        elif i<j:
+            return op2
     # def Forward_index(self, input=[], index=0):
     #     for i in range(index, len(input)):
     #         if input[i]==')':
     #             index += 1
     #     return index
-    def Algorithm(self, input=[], Temp=[], Number=[], i=0):
-        Number = []
-        Temp = []
-        h = len(input)
-        while i <= h:
+    def Algorithm(self, input=[], index=0):
+        for i in range(index, len(input)):
+            print("i = ",  i)
             if input[i]=='(':
                 self.Character.append(input[i])
                 self.Print(input[i])
-                hasil = self.Algorithm(input, Temp, Number, i)
-                # index += self.Forward_index(input, i+1)
-                Number.append(hasil)
+                input.append(self.Algorithm(input, i+1))
+                # i += self.Forward_index(input, i+1)
+                print("i diubah menjadi : ", i)
                 # i += index
             elif input[i].isdigit():
-                Number.append(input[i])
                 self.Postfix.append(input[i])
             elif self.Is_Operator(input[i]) is True:
-                Temp.append(input[i])
-                self.Character.append(input[i])
+                if self.Is_Operator(self.Character[-1]) is True:
+                    self.Postfix.append(self.Replace_operator(self.Character.pop(), input[i]))
+                else:
+                    self.Character.append(input[i])
             elif input[i]==')':
-                return self.Priority(Temp, Number)
+                return self.Priority(input, index)
             elif input[i]==input[-1]:
-                return self.Priority(Temp, Number)
+                return self.Priority(input, index)
             self.Print(input[i])
-            print(Temp, Number, "\n")
-            i += 1
 
 input = "1+(2/3-(4*5^6)+7)*8"
-Temp = []
-Number = []
 a = Infix_Postfix()
 a.Convert(input)
-hasil = a.Algorithm(a.Input, Temp, Number)
+# print(a.Input)
+hasil = a.Algorithm(a.Input)
 print("\nhasilnya : ", hasil)
--- coba.py
+++ coba.py
@@ -1,4 +1,13 @@
 class Coba:
+    def __init__(self):
+        self.Input = []
+    def Convert(self, input):
+        for char in input:
+            self.Input.append(char)
+    def Is_Operator(self, character):
+        operators = "+-*/^"
+        if character in operators:
+            return True
     def Operation(self, var1, var2, operator):
         if operator=='+':
             return int(var1)+int(var2)
@@ -10,11 +19,23 @@ class Coba:
             return int(var1)/int(var2)
         elif operator=='^':
             return int(var1)**int(var2)
-    def Priority(self, Temp=[], Number=[]):
-        while len(Number) != 1:
-            best = 0
-            i = 0
-            for j, operator in enumerate(Temp):
+    def Priority(self, input=[], index=0):
+        self.Char = []
+        self.Num = []
+        print(input, len(input))
+        while input:
+            print(index, input[index])
+            if input[index]==')':
+                break
+            elif input[index].isdigit():
+                self.Num.append(input.pop(index))
+            elif self.Is_Operator(input[index]) is True:
+                self.Char.append(input.pop(index))
+            print(self.Char, self.Num)
+        i = 0
+        best  = 0 
+        while len(self.Num) != 1:
+            for j, operator in enumerate(self.Char):
                 if operator=='+' or operator=='-' and best < 1:
                     n = 1
                     i = j
@@ -25,13 +46,14 @@ class Coba:
                     n = 3
                     i = j
                 best = n
-            print("var1 :", Number[i], "var2 :", Number[i+1], "oper :", Temp[i])
-            hasil = self.Operation(Number.pop(i), Number.pop(i), Temp.pop(i))
-            Number.append(hasil)
-        return Number.pop()
+            print(i)
+            print("var1 :", self.Num[i], "var2 :", self.Num[i+1], "oper :", self.Char[i])
+            hasil = self.Operation(self.Num.pop(i), self.Num.pop(i), self.Char.pop(i))
+            self.Num.append(hasil)
+        input[index] = self.Num.pop()
+        print(input)
 
-temp = ['*', '^']
-number = ['4', '5', '6']
+input = "1+2*3^4)+7"
 a = Coba()
-hasil = a.Priority(temp, number)
-print("Hasilnya = ", hasil)
+a.Convert(input)
+a.Priority(a.Input)
